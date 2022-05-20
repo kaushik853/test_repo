0fe03a2c6d3a478c05d4157096530f1d5eb0710c
kaushik853 <kaushik.pal@adidas.com>
2022-05-20 17:53:38 +0200

    added a test func

--- test_repo.py
+++ test_repo.py
@@ -1,3 +1,25 @@
 #! /usr/bin/env python
 
 import requests
+import json
+from pprint import pprint
+from github import Github
+
+def user():
+    username = 'kaushik853'
+    req_url = f"https://api.github.com/users/{username}"
+    
+    user_data = requests.get(req_url).json()
+    
+    pprint(user_data)
+
+def user_repo():
+    username = 'kaushik853'
+    g = Github()
+    user = g.get_user(username)
+    for repo in user.get_repos():
+        print(repo)
+
+if __name__=="__main__":
+    user_repo()
+
